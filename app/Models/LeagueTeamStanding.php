2f57f98aed9585ddd20bbfdbe8aa1cfa85e81ac5
Laith Atik <laithatik@gmail.com>
2022-04-16 23:19:24 +0300

    add fillable to LeagueTeamStanding model

--- app/Models/LeagueTeamStanding.php
+++ app/Models/LeagueTeamStanding.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class LeagueTeamStanding extends Pivot
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'played',
+        'won',
+        'drawn',
+        'lost',
+        'goals_for',
+        'goals_against',
+        'goals_difference',
+        'points',
+    ];
 }
